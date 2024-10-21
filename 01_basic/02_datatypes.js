0b91067e2c6060c996f6f37097c632042fe0c8c4
Vaibhavv17 <30970603+Vaibhavv17@users.noreply.github.com>
2024-10-21 14:10:11 +0000

    not entire datatype

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "hitesh" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+
+// number => 2 to power 53
+// bigint for greater than this,very rare, stocks,bigwebsite
+// string "" or '',but ""
+// boolean => true or false
+// null => standalone value or represents a empty value 
+// undefined => value not defined.
+// symbol => in react, to find uniqueness in components
