8d155fc9b031fc87b59062dc0efb31f37f67868d
Vaibhavv17 <30970603+Vaibhavv17@users.noreply.github.com>
2024-10-22 13:16:34 +0000

    not complete nums and maths

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400 //automatically defined
+
+const balance = new Number(100)
+console.log(balance);
+console.log(score);
+
+console.log(balance.toString().length); //by converting to string i can use string method and properties
+
+console.log(balance.toFixed(2)); //2 decimal points
+
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(3));
+
+
+
